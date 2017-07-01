46e8757a24664c5cb9ee47d30f671eee2828bba6
Korede Folarin <korede@localhost.localdomain>
2017-07-02 00:50:25 +0100

    completed plot4

--- plot1.R
+++ plot1.R
@@ -10,6 +10,8 @@ library(dplyr)
 
 hpow <- filter(mutate(fread("household_power_consumption.txt", na.strings="?", header=TRUE), date2:= as.POSIXct(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"))), date2 >= as.POSIXct("2007-02-01"), date2 < as.POSIXct("2007-02-03"))
 
+plot.new()
+
 png(filename = "plot1.png", width=480, height=480)
 
 hist(hpow$Global_active_power, main="Global Active Power", xlab="Global Active Power (killowatts)", col="red")
--- plot4.R
+++ plot4.R
@@ -12,17 +12,28 @@ hpow <- filter(mutate(fread("household_power_consumption.txt", na.strings="?", h
 
 plot.new()
 
-png(filename = "plot3.png", width=480, height=480)
+png(filename = "plot4.png", width=480, height=480)
 
-# set up the chart / device but no plot
+# set up canvas for 2 x 2 chart layout
+par(mfrow=c(2,2))
+
+# top left chart
+with(hpow,plot(date2, Global_active_power, type="l", ylab="Global Active Power", xlab=""))
+
+# top right chart
+with(hpow,plot(date2, Voltage, type="l", ylab="Voltage", xlab="datetime"))
+
+# bottom left chart
 with(hpow, plot(date2, Sub_metering_1, type="n", xlab="", ylab = "Energy sub metering"))
 
-# plot lines on graph
 points(hpow$date2, hpow$Sub_metering_1, type="l", col="black")
 points(hpow$date2, hpow$Sub_metering_2, type="l", col="red")
 points(hpow$date2, hpow$Sub_metering_3, type="l", col="blue")
 
 # add top right legend
-legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1)
+legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1, bty="n")
+
+#bottom right chart
+with(hpow,plot(date2, Global_reactive_power, type="l", xlab="datetime"))
 
 dev.off()
