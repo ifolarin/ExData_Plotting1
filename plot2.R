3ef9e793ff4bf85c0dd1f51344c459168a6f7858
Korede Folarin <korede@localhost.localdomain>
2017-07-01 16:12:15 +0100

    Finished Plot2

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 zipFile <- "../household_power_consumption.zip"
 
 if(!file.exists(zipFile)){
-  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="household_power_consumption.zip")
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest=zipFile)
 }
   
 unzip(zipFile)
@@ -10,8 +10,8 @@ library(dplyr)
 
 hpow <- filter(mutate(fread("household_power_consumption.txt", na.strings="?", header=TRUE), date2:= as.POSIXct(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"))), date2 >= as.POSIXct("2007-02-01"), date2 < as.POSIXct("2007-02-03"))
 
-png(filename = "plot1.png", width=480, height=480)
+png(filename = "plot2.png", width=480, height=480)
 
-hist(hpow$Global_active_power, main="Global Active Power", xlab="Global Active Power (killowatts)", col="red")
+with(hpow,plot(date2, Global_active_power, type="l", ylab="Global Active Power (kilowatts)"))
 
 dev.off()
